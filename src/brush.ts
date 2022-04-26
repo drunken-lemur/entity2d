bd10db5faba2a58200285969b37928ee373aa3be
Alexey Karasev <alex.deimos@emidao.org>
2022-04-26 20:22:24 +0700

    Added export getBrush from brush2d

--- src/brush.ts
+++ src/brush.ts
@@ -1,6 +1,8 @@
-import type { IBrush as IBrush2D } from 'brush2d';
+import type { IBrush as IBrush2D, getBrush } from 'brush2d';
 
 export interface IBrush extends IBrush2D {
   save: () => this;
   restore: () => this;
 }
+
+export type { getBrush };
