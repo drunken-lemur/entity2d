fc7fd4ddce45441ea24d9f9b7271da77d77cac6a
Alexey Karasev <alex.deimos@emidao.org>
2022-04-26 20:25:21 +0700

    Fixed exports

--- src/brush.ts
+++ src/brush.ts
@@ -1,8 +1,8 @@
-import type { IBrush as IBrush2D, getBrush } from 'brush2d';
+import { IBrush as IBrush2D, getBrush } from 'brush2d';
 
 export interface IBrush extends IBrush2D {
   save: () => this;
   restore: () => this;
 }
 
-export type { getBrush };
+export { getBrush };
